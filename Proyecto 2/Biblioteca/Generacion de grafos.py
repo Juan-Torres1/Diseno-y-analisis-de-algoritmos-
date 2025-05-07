04091b6d4cf511004fd2222ba0ef30ec43d62d12
Juan-Torres1 <815torres@gmail.com>
2025-05-07 12:43:58 -0600

    Update Generacion de grafos.py

--- Proyecto 2/Biblioteca/Generacion de grafos.py
+++ Proyecto 2/Biblioteca/Generacion de grafos.py
@@ -1,76 +1,44 @@
-import Algoritmos as a 
+import Algoritmos as a
 
 def main():
     try:
 
-       #Malla
-       #30 nodos
-       grafogridGraph=a.gridGraph(10, 5, directed=False)
-       grafogridGraph.save_to_graphviz('Malla50')
-       #200 nodos
-       grafogridGraph=a.gridGraph(10, 20, directed=False)
-       grafogridGraph.save_to_graphviz('Malla200')
-       #500 nodos
-       grafogridGraph=a.gridGraph(10, 50, directed=False)
-       grafogridGraph.save_to_graphviz('Malla500')
+        #Nodo raiz para algoritmos BFS, DFS Interativo y DFS Recirsivo
+        rootNode='0:0'
+        #Malla
 
-       #Erdos
-       #30 nodos
-       grafoErdos=a.ErdosRenyiGraph(50, 50, directed=False)
-       grafoErdos.save_to_graphviz('Erdos50')
-       #200 nodos
-       grafoErdos=a.ErdosRenyiGraph(200, 200, directed=False)
-       grafoErdos.save_to_graphviz('Erdos200')
-       #500 nodos
-       grafoErdos=a.ErdosRenyiGraph(500, 500, directed=False)
-       grafoErdos.save_to_graphviz('Erdos500')
-
-       #Gilbert
-       #30 nodos
-       grafoGilbert=a.gilbertGraph(50, 0.02, directed=False)
-       grafoGilbert.save_to_graphviz('Gilbert50')
-       #200 nodos
-       grafoGilbert=a.gilbertGraph(200, 0.01, directed=False)
-       grafoGilbert.save_to_graphviz('Gilbert200')
-       #500 nodos
-       grafoGilbert=a.gilbertGraph(500, 0.01, directed=False)
-       grafoGilbert.save_to_graphviz('Gilbert500')
-
-       #Geografico
-       #30 nodos
-       geographicGraph=a.geographicGraph(50, 0.5, directed=False)
-       geographicGraph.save_to_graphviz('Geografico50')
-       #200 nodos
-       geographicGraph=a.geographicGraph(200, 0.3, directed=False)
-       geographicGraph.save_to_graphviz('Geografico200')
-       #500 nodos
-       geographicGraph=a.geographicGraph(500, 0.1, directed=False)
-       geographicGraph.save_to_graphviz('Geografico500')
-
-       #Barabasi
-       #30 nodos
-       grafoBarabasiAlbert=a.grafoBarabasiAlbert(50, 2, dirigido=False)
-       grafoBarabasiAlbert.save_to_graphviz('BarabasiAlbert50')
-       #200 nodos
-       grafoBarabasiAlbert=a.grafoBarabasiAlbert(200, 3, dirigido=False)
-       grafoBarabasiAlbert.save_to_graphviz('BarabasiAlbert200')
-       #500 nodos
-       grafoBarabasiAlbert=a.grafoBarabasiAlbert(500, 5, dirigido=False)
-       grafoBarabasiAlbert.save_to_graphviz('BarabasiAlbert500')
-
-       #Dorogovtsev
-       #30 nodos
-       grafoDorogovtsevMendes=a.grafoDorogovtsevMendes(n=50, dirigido=False)
-       grafoDorogovtsevMendes.save_to_graphviz('DorogovtsevMendes50')
-       #200 nodos
-       grafoDorogovtsevMendes=a.grafoDorogovtsevMendes(n=200, dirigido=False)
-       grafoDorogovtsevMendes.save_to_graphviz('DorogovtsevMendes200')
-       #500 nodos
-       grafoDorogovtsevMendes=a.grafoDorogovtsevMendes(n=500, dirigido=False)
-       grafoDorogovtsevMendes.save_to_graphviz('DorogovtsevMendes500')
+        #30 Nodos  
+        grafogridGraph=a.gridGraph(6, 5, directed=False)
+        grafogridGraph.save_to_graphviz('Malla30')
+        grafogridGraphBFS=grafogridGraph.bfs(rootNode)
+        grafogridGraph.save_to_graphviz('Malla30bfs')
+        grafogridGraphBFS=grafogridGraph.dfs_iterative(rootNode)
+        grafogridGraph.save_to_graphviz('Malla30dfs_iterativo')
+        grafogridGraphBFS=grafogridGraph.dfs_recursive(rootNode)
+        grafogridGraph.save_to_graphviz('Malla30dfs_recursivo')
+        print("Nodos del grafo:", grafogridGraph.nodes)
+        #100 Nodos
+        grafogridGraph=a.gridGraph(10, 10, directed=False)
+        grafogridGraph.save_to_graphviz('Malla100')
+        grafogridGraphBFS=grafogridGraph.bfs(rootNode)
+        grafogridGraph.save_to_graphviz('Malla100bfs')
+        grafogridGraphBFS=grafogridGraph.dfs_iterative(rootNode)
+        grafogridGraph.save_to_graphviz('Malla100dfs_iterativo')
+        grafogridGraphBFS=grafogridGraph.dfs_recursive(rootNode)
+        grafogridGraph.save_to_graphviz('Malla100dfs_recursivo')
+        print("Nodos del grafo:", grafogridGraph.nodes)
+        #500 nodos
+        grafogridGraph=a.gridGraph(50, 10, directed=False)
+        grafogridGraph.save_to_graphviz('Malla500')
+        grafogridGraphBFS=grafogridGraph.bfs(rootNode)
+        grafogridGraph.save_to_graphviz('Malla500bfs')
+        grafogridGraphBFS=grafogridGraph.dfs_iterative(rootNode)
+        grafogridGraph.save_to_graphviz('Malla500dfs_iterativo')
+        grafogridGraphBFS=grafogridGraph.dfs_recursive(rootNode)
+        grafogridGraph.save_to_graphviz('Malla500dfs_recursivo')
+        print("Nodos del grafo:", grafogridGraph.nodes)
 
     except Exception as err:
         print(err)
 
 main()
-
